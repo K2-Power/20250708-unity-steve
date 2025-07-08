e390c9a54859648f610812319a6b75327c4f0d18
MAETEL\2247041 <2247041@ecc.ac.jp>
2025-07-08 11:32:22 +0900

    ボタンのスクリプトとゴールのスクリプトの追加

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -4,15 +4,21 @@ using UnityEngine.SceneManagement;
 public class Button : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+
+    // Dead‚ÉG‚ê‚Ä‚¢‚é‚©‚Ç‚¤‚©‚Ìó‘Ô
+    public bool isTouchingDead { get; private set; } = false;
+
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Dead"))
         {
             spriteRenderer.enabled = false;
+            isTouchingDead = true;
         }
     }
 
@@ -21,6 +27,7 @@ public class Button : MonoBehaviour
         if (other.CompareTag("Dead"))
         {
             spriteRenderer.enabled = true;
+            isTouchingDead = false;
         }
     }
 }
--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
+    public Button buttonScript; // インスペクターでButtonのスクリプトをドラッグして渡す
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player") && buttonScript.isTouchingDead)
         {
             SceneManager.LoadScene("GameScene2");
         }
