81d422d80d96643d61720a5d369dd2b7bfebb89b
MAETEL\2247041 <2247041@ecc.ac.jp>
2025-07-15 10:24:46 +0900

    ゴールボタンとステージボタンの分別と、ゲームシーンの移行を少しいじりますた

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
-    public Button buttonScript; // インスペクターでButtonのスクリプトをドラッグして渡す
+    public GoalButton GoalButton; // インスペクターでButtonのスクリプトをドラッグして渡す
+    public string sceneName; // インスペクターで指定するシーン名（例："GameScene"）
+    public void ChangeScene()
+    {
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning("sceneNameが指定されていません！");
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && buttonScript.isTouchingDead)
+        if (other.CompareTag("Player") && GoalButton.isTouchingDead)
         {
-            SceneManager.LoadScene("GameScene2");
+            ChangeScene();
         }
     }
 }
---
+++ Assets/Script/GoalButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalButton : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    // Dead‚ÉG‚ê‚Ä‚¢‚é‚©‚Ç‚¤‚©‚Ìó‘Ô
+    public bool isTouchingDead { get; private set; } = false;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Dead"))
+        {
+            spriteRenderer.enabled = false;
+            isTouchingDead = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Dead"))
+        {
+            spriteRenderer.enabled = true;
+            isTouchingDead = false;
+        }
+    }
+}
