2ea63689721ecc47bb8daed8d94bbf2159dc0132
絹川 <2247031@ecc.ac.jp>
2025-07-16 16:06:18 +0900

    シーンとゴールボタンの変更

--- Assets/Script/Goal.cs
+++ Assets/Script/Goal.cs
@@ -4,11 +4,11 @@ using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {   
     public string sceneName; // インスペクターで指定するシーン名（例："GameScene"）
-    public GoalButton GoalButton;
+   
   
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && GoalButton.isTouchingDead== true)
+        if (other.CompareTag("Player") && Button.instance.isTouchingDead == true)
         {
             SceneManager.LoadScene(sceneName);
         }
