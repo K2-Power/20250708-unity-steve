05013653fd6484d32e54faf652cb0683bc6b31a0
MAETEL\2247041 <2247041@ecc.ac.jp>
2025-07-16 16:09:58 +0900

    リセット機能追加

---
+++ Assets/Script/Reset.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーンの管理に必要
+
+public class Reset : MonoBehaviour
+{
+    // Update関数は毎フレーム呼ばれる
+    void Update()
+    {
+        // 「R」キーが押されたらリセット
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetGame(); // リセット関数を呼び出す
+        }
+    }
+
+    // ゲームのリセット処理（シーンの再読み込み）
+    void ResetGame()
+    {
+        // 現在のアクティブなシーンを取得
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // そのシーンを再読み込みしてリセット
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
