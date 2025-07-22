5d87517a93d36083ce3a04ecb3b6eca4bd33a2a4
絹川 <2247031@ecc.ac.jp>
2025-07-22 09:16:10 +0900

    CloneCountのUI

---
+++ Assets/Script/CloneCountScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CloneCountScript : MonoBehaviour
+{
+    public TextMeshProUGUI CountText;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountText.text = Player.instance.CloneCount.ToString();
+    }
+}
