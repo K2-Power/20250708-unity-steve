e2bafc3729c68957c950c5128aba8d744299a464
絹川 <2247031@ecc.ac.jp>
2025-08-05 09:07:08 +0900

    タイトルとsoundフォルダ追加、bgmファイル追加

--- Assets/Script/TitleManager.cs
+++ Assets/Script/TitleManager.cs
@@ -3,15 +3,38 @@ using UnityEngine.SceneManagement; // シーンの管理に必要
 
 public class TitleManager : MonoBehaviour
 {
+    public GameObject title;
+    public GameObject title1;
+    private int counter;
+    public int Maxcounter;
+
+    void Start()
+    {
+         counter = 0;
+        title1.SetActive(false);
+    }
     void Update()
     {
         if(Input.GetKeyDown("joystick button 2"))
         {
-            SceneManager.LoadScene("TutorialScene");
+            
+            counter++;
         }
         else if (Input.GetMouseButtonDown(1))
         {
-            SceneManager.LoadScene("TutorialScene");
+            counter++;
+        }
+        switch (counter)
+        {
+            case 1:
+                title1.SetActive(false);
+                title1.SetActive(true);
+                break;
+            case 2:
+                SceneManager.LoadScene("GameScene1");
+                break;
+            default:
+                break;
         }
     }
 }
