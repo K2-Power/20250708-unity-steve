185cb1edd6dfcd1a204b1dbf211b8e47d820f5ff
MAETEL\2247041 <2247041@ecc.ac.jp>
2025-10-02 09:56:17 +0900

    ゲームシーン６!!!!!!!!

--- Assets/Script/StageButton.cs
+++ Assets/Script/StageButton.cs
@@ -1,15 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StageButton : MonoBehaviour
 {
-    public GameObject ButtonObject;
+    public List<GameObject> ButtonObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Destroy(ButtonObject);
+            DestroyAll();
             Destroy(gameObject);
         }
+    }
+    public void DestroyAll()
+    {
+        foreach (GameObject obj in ButtonObject)
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+            }
+        }
+        // リスト自体をクリア
+        ButtonObject.Clear();
     }
 }
